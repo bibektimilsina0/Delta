a63ac1d80c568adfac3a10fe48faa0dd22536544
Ashim-Stha <ashimshrestha2384@gmail.com>
2024-01-20 22:17:55 +0545

    final

--- NodeAPI.py
+++ NodeAPI.py
@@ -5,6 +5,7 @@ from urllib.parse import unquote
 from Wallet import Wallet
 import requests
 
+from flask_cors import CORS
 
 node = None
 
@@ -12,12 +13,14 @@ node = None
 class NodeAPI(FlaskView):
 
     bob = Wallet()
+    bob.fromKey('keys/bobPrivateKey.pem')
     alice = Wallet()
     alice.fromKey('keys/stakerPrivateKey.pem')
     exchnage=Wallet()
 
     def __init__(self):
         self.app = Flask(__name__)
+        CORS(self.app)
 
     def start(self, port):
         NodeAPI.register(self.app, route_base='/')
@@ -100,8 +103,8 @@ class NodeAPI(FlaskView):
         print('bob',balanceBob)
         print('alice',balanceAlice)
         
-        response={'bob':balanceBob,'alice':balanceAlice,'data':e}
-        return jsonify(response),200
+        
+        return jsonify(e),200
     
 
     @route('/balance', methods=['POST'])
@@ -114,3 +117,119 @@ class NodeAPI(FlaskView):
         balance = node.blockchain.getAccountBalance(public_key)
         response = {'balance': balance}
         return jsonify(response), 200
+# from flask_classful import FlaskView, route
+# from flask import Flask, jsonify, request
+# from BlockchainUtils import BlockchainUtils
+# from flask_cors import CORS
+# from urllib.parse import unquote
+# from Wallet import Wallet
+# import requests
+
+
+# node = None
+
+
+# class NodeAPI(FlaskView):
+
+#     bob = Wallet()
+#     alice = Wallet()
+#     alice.fromKey('keys/stakerPrivateKey.pem')
+#     exchnage=Wallet()
+
+#     def __init__(self):
+#         self.app = Flask(__name__)
+#         CORS(self.app)
+
+#     def start(self, port):
+#         NodeAPI.register(self.app, route_base='/')
+#         self.app.run(host='localhost', port=port)
+
+#     def injectNode(self, injectedNode):
+#         global node
+#         node = injectedNode
+
+#     @route('/info', methods=['GET'])
+#     def info(self):
+#         return 'This is a communiction interface to a nodes blockchain', 200
+
+#     @route('/blockchain', methods=['GET'])
+#     def blockchain(self):
+#         return node.blockchain.toJson(), 200
+
+#     @route('/transactionPool', methods=['GET'])
+#     def transactionPool(self):
+#         transactions = {}
+#         for ctr, transaction in enumerate(node.transactionPool.transactions):
+#             transactions[ctr] = transaction.toJson()
+#         return jsonify(transactions), 200
+    
+
+    
+
+#     @route('/transaction', methods=['POST'])
+#     def transaction(self):
+#         values = request.get_json()
+#         if not 'transaction' in values:
+#             return 'Missing transaction value', 400
+#         transaction = BlockchainUtils.decode(values['transaction'])
+#         node.handleTransaction(transaction)
+#         response = {'message': 'Received transaction'}
+#         return jsonify(response), 201
+    
+    
+    
+#     @route('/initi', methods=['POST'])
+#     def initi(self):
+#         def postTransaction(sender, receiver, amount, type):
+#             transaction = sender.createTransaction(
+#                 receiver.publicKeyString(), amount, type)
+#             url = "http://localhost:5000/transaction"
+#             package = {'transaction': BlockchainUtils.encode(transaction)}
+#             requests.post(url, json=package)
+            
+        
+#         postTransaction(self.exchnage, self.alice, 100, 'EXCHANGE')
+        
+
+#         balanceBob = node.blockchain.getAccountBalance(self.bob.publicKeyString())
+#         balanceAlice= node.blockchain.getAccountBalance(self.alice.publicKeyString())
+#         print('bob',balanceBob)
+#         print('alice',balanceAlice)
+        
+#         response={'bob':balanceBob,'alice':balanceAlice}
+#         return jsonify(response),200
+    
+#     @route('/do', methods=['POST'])
+#     def do(self):
+#         values = request.get_json()
+#         if 'amount' not in values or 'type' not in values:
+#             return 'Missing amount or type value', 400
+#         def postTransaction(sender, receiver, amount, type):
+#             transaction = sender.createTransaction(
+#                 receiver.publicKeyString(), amount, type)
+#             url = "http://localhost:5000/transaction"
+#             package = {'transaction': BlockchainUtils.encode(transaction)}
+#             requests.post(url, json=package)
+#             return package
+        
+#         e=postTransaction(self.alice,self.bob,values['amount'],values['type'])
+
+#         balanceBob = node.blockchain.getAccountBalance(self.bob.publicKeyString())
+#         balanceAlice= node.blockchain.getAccountBalance(self.alice.publicKeyString())
+#         print('bob',balanceBob)
+#         print('alice',balanceAlice)
+        
+#         response={'bob':balanceBob,'alice':balanceAlice,'data':e}
+#         return jsonify(response),200
+    
+
+#     @route('/balance', methods=['POST'])
+#     def get_balance(self):
+#         values = request.get_json()
+#         if 'public_key' not in values:
+#             return 'Missing public_key value', 400
+
+#         public_key = values['public_key']
+#         balance = node.blockchain.getAccountBalance(public_key)
+#         response = {'balance': balance}
+#         return jsonify(response), 200
